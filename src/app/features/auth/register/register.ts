7ad7a57ed27d561164d78379aaed97365531060b
Dobromir Todorov <dobromirtt@gmail.com>
2025-08-07 01:08:57 +0300

    Basic registration form validation implemented.

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -10,94 +10,116 @@ import { AuthService } from '../../../core/services';
   styleUrl: './register.css'
 })
 export class Register {
+
+  private formBuilder = inject(FormBuilder);
+
+  registerFormGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerFormGroup = this.fb.group({
+      username: ['justberoe', [
+        Validators.required
+        // ,
+        // Validators.minLength(5)
+      ]],
+      email: ['dobromirtt@gmail.com', Validators.required],
+      password: 123,
+      rePassword: 123
+      // ,
+      // operatingSystem: ''
+    })
+  }
+
+
+  //OLD validaiton without 
   private authService = inject(AuthService);
   private router = inject(Router);
 
-  username: string = '';
-  email: string = '';
-  phone: string = '';
-  password: string = '';
-  rePassword: string = '';
+  // username: string = '';
+  // email: string = '';
+  // phone: string = '';
+  // password: string = '';
+  // rePassword: string = '';
 
-  usernameError: boolean = false;
-  emailError: boolean = false;
-  phoneError: boolean = false;
-  passwordError: boolean = false;
-  rePasswordError: boolean = false;
+  // usernameError: boolean = false;
+  // emailError: boolean = false;
+  // phoneError: boolean = false;
+  // passwordError: boolean = false;
+  // rePasswordError: boolean = false;
 
-  usernameErrorMessage: string = '';
-  emailErrorMessage: string = '';
-  phoneErrorMessage: string = '';
-  passwordErrorMessage: string = '';
-  rePasswordErrorMessage: string = '';
+  // usernameErrorMessage: string = '';
+  // emailErrorMessage: string = '';
+  // phoneErrorMessage: string = '';
+  // passwordErrorMessage: string = '';
+  // rePasswordErrorMessage: string = '';
 
-  validateUsername(): void {
+  // validateUsername(): void {
 
-    if (!this.email) {
-      this.usernameError = true;
-      this.usernameErrorMessage = 'Username is required!';
-    } else {
-      this.usernameError = false;
-      this.usernameErrorMessage = '';
-    }
-  }
+  //   if (!this.email) {
+  //     this.usernameError = true;
+  //     this.usernameErrorMessage = 'Username is required!';
+  //   } else {
+  //     this.usernameError = false;
+  //     this.usernameErrorMessage = '';
+  //   }
+  // }
 
-  validateEmail(): void {
-    if (!this.email) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is required!';
-    } else if (!this.isValidEmail(this.email)) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is not valid!';
+  // validateEmail(): void {
+  //   if (!this.email) {
+  //     this.emailError = true;
+  //     this.emailErrorMessage = 'Email is required!';
+  //   } else if (!this.isValidEmail(this.email)) {
+  //     this.emailError = true;
+  //     this.emailErrorMessage = 'Email is not valid!';
 
-    } else {
-      this.emailError = false;
-      this.emailErrorMessage = '';
-    }
-  }
+  //   } else {
+  //     this.emailError = false;
+  //     this.emailErrorMessage = '';
+  //   }
+  // }
 
-  validatePhone(): void {
-    if (!this.email) {
-      this.phoneError = true;
-      this.phoneErrorMessage = 'Phone is required!';
-    } else {
-      this.phoneError = false;
-      this.phoneErrorMessage = '';
-    }
-  }
-  validatePassword(): void {
-    if (!this.password) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password is required!';
-    }
-    else if (this.password.length < 4) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password must be at least 4 characters!';
-    } else {
-      this.passwordError = false;
-      this.passwordErrorMessage = '';
-    }
+  // validatePhone(): void {
+  //   if (!this.email) {
+  //     this.phoneError = true;
+  //     this.phoneErrorMessage = 'Phone is required!';
+  //   } else {
+  //     this.phoneError = false;
+  //     this.phoneErrorMessage = '';
+  //   }
+  // }
+  // validatePassword(): void {
+  //   if (!this.password) {
+  //     this.passwordError = true;
+  //     this.passwordErrorMessage = 'Password is required!';
+  //   }
+  //   else if (this.password.length < 4) {
+  //     this.passwordError = true;
+  //     this.passwordErrorMessage = 'Password must be at least 4 characters!';
+  //   } else {
+  //     this.passwordError = false;
+  //     this.passwordErrorMessage = '';
+  //   }
 
-    if (this.rePassword) {
-      this.validateRePassword();
-    }
+  //   if (this.rePassword) {
+  //     this.validateRePassword();
+  //   }
 
-  }
-  validateRePassword(): void {
-    if (!this.rePassword) {
-      this.rePasswordError = true;
-      this.rePasswordErrorMessage = 'Repeaset Password is required!';
-    }
-    else if (this.password !== this.rePassword) {
-      this.rePasswordError = true;
-      this.rePasswordErrorMessage = 'Repeat password does not match password!';
-    } else {
-      this.rePasswordError = false;
-      this.rePasswordErrorMessage = '';
-    }
+  // }
+  // validateRePassword(): void {
+  //   if (!this.rePassword) {
+  //     this.rePasswordError = true;
+  //     this.rePasswordErrorMessage = 'Repeaset Password is required!';
+  //   }
+  //   else if (this.password !== this.rePassword) {
+  //     this.rePasswordError = true;
+  //     this.rePasswordErrorMessage = 'Repeat password does not match password!';
+  //   } else {
+  //     this.rePasswordError = false;
+  //     this.rePasswordErrorMessage = '';
+  //   }
 
 
-  }
+  // }
 
   isFormValid(): boolean {
 
@@ -122,20 +144,31 @@ export class Register {
     // this.validatePassword();
     // this.validateRePassword;
 
-    if (this.isFormValid()) {
+      if (this.registerFormGroup.valid)  {
+
+      const formValues = this.registerFormGroup.value;
+      console.log('Submitted form values:', formValues);
+
+      // Destructure values
+      const { username, email, password, rePassword } = formValues;
+      // Optional: Add manual validation for matching passwords
+      if (password !== rePassword) {
+        alert("Passwords do not match.");
+        return;
+      }
       const response = this.authService.register(
-        this.username,
-        this.email,
+        username,
+        email,
         // this.phone,
-        this.password,
-        this.rePassword);
+        password,
+        rePassword);
 
       // Call MongoDB backend via HTTP
       this.authService.registerInMongo(
-        this.username,
-        this.email,
-        this.password,
-        this.rePassword
+        username,
+        email,
+        password,
+        rePassword
       ).subscribe({
         next: (res) => {
           console.log('User saved in MongoDB:', res);
@@ -143,21 +176,21 @@ export class Register {
         },
         error: (err) => {
           console.error('MongoDB registration failed:', err);
-          alert('Registration failed. Try again.');
+          alert('Registration failed. Try again.' + err);
         }
       });
-    
 
 
-    if (response === true) {
-      this.router.navigate(['/home']);
+
+      if (response === true) {
+        this.router.navigate(['/home']);
+      }
     }
   }
-}
 
-  private isValidEmail(email: string): boolean {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-  // const emailRegex = /^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/;
-  return emailRegex.test(email);
-}
+  // private isValidEmail(email: string): boolean {
+  //   const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+  //   // const emailRegex = /^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/;
+  //   return emailRegex.test(email);
+  // }
 }
